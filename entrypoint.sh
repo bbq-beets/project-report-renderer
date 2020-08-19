34408104a2f9ecb7f878767120a11875b39701ae
Jonathan Clem <jclem@github.com>
2020-08-19 12:07:47 -0400

    Change input names to use camelCase

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ set -e
 build_dir=/opt/build
 
 # Copy the reports from the working directory to our build location
-cp -R $INPUT_REPORTS_PATH $build_dir/.reports
+cp -R $INPUT_REPORTSPATH $build_dir/.reports
 
 cd $build_dir
 npm run build
-npm run export -- -o "$GITHUB_WORKSPACE/$INPUT_OUTPUTS_PATH"
+npm run export -- -o "$GITHUB_WORKSPACE/$INPUT_OUTPUTSPATH"
--- test.sh
+++ test.sh
@@ -8,8 +8,8 @@ workdir=/home/github
 docker build . -t $tag
 
 docker run \
-  -e INPUT_REPORTS_PATH=./.reports \
-  -e INPUT_OUTPUTS_PATH=./docs \
+  -e INPUT_REPORTSPATH=./.reports \
+  -e INPUT_OUTPUTSPATH=./docs \
   -e GITHUB_WORKSPACE=$workdir \
   -w=$workdir \
   --mount type=bind,src=$(pwd),dst=$workdir \

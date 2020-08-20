f43357cb193c8e1926797536537807d052692b8c
Jonathan Clem <jclem@github.com>
2020-08-20 09:14:29 -0400

    Use the build directory for Next.js's build cache

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,8 @@ build_dir=/opt/build
 
 # Copy the reports from the working directory to our build location
 cp -R $INPUT_REPORTSPATH $build_dir/.reports
+# Move the outputs path to the build location so we can use the cached build
+mv $GITHUB_WORKSPACE/$INPUT_OUTPUTSPATH $build_dir
 
 cd $build_dir
 npm run build

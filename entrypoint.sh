5f20d2291265a26cfb72122bd0d200ae082f4c7e
Jonathan Clem <jclem@github.com>
2020-08-24 14:13:28 -0400

    Remove Pages build request

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,11 +15,6 @@ npm run build
 npm run export -- -o $GITHUB_WORKSPACE/$INPUT_OUTPUTSPATH
 
 if [[ -z "$INPUT_NOCOMMIT" ]]; then
-  if [[ -z "$INPUT_GITHUBTOKEN" ]]; then
-    echo "No githubToken input provided, can't request page build."
-    exit 1
-  fi
-
   # Commit the changes.
   cd $GITHUB_WORKSPACE
   git config user.name github-actions
@@ -27,17 +22,4 @@ if [[ -z "$INPUT_NOCOMMIT" ]]; then
   git add $INPUT_OUTPUTSPATH
   git commit -m "Generated build from workflow run: $GITHUB_RUN_ID"
   git push
-
-  # Request a GitHub Pages build.
-  http_code=$(curl -s -w "%{http_code}" -o /tmp/curl-out.html -X POST \
-    https://api.github.com/repos/$GITHUB_REPOSITORY/pages/builds \
-    -H "Accept: application/vnd.github.v3+json" \
-    -H "Authorization: Bearer $INPUT_GITHUBTOKEN" \
-    -H "User-Agent: bbq-beets/project-report-renderer")
-  
-  if [[ ! "$http_code" == "201" ]]; then
-    echo "Expected 201, got $http_code from build request. Output:"
-    cat /tmp/curl-out.html
-    exit 1
-  fi
 fi

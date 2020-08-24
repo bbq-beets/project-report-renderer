706e19570b6ce8be724fc8c7fdfd755375276aac
Jonathan Clem <jclem@github.com>
2020-08-24 12:50:59 -0400

    Commit changes when complete

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
 
-set -e
+set -ve
 
 build_dir=/opt/build
 
-# Copy the reports from the working directory to our build location
+# Copy the reports from the working directory to our build location.
 cp -R $INPUT_REPORTSPATH $build_dir/.reports
-# Move the outputs path to the build location so we can use the cached build
+# Move the outputs path to the build location so we can use the cached build.
 mv $GITHUB_WORKSPACE/$INPUT_OUTPUTSPATH $build_dir
 
+# Export back to the workspace output path.
 cd $build_dir
 npm run build
-npm run export -- -o "$GITHUB_WORKSPACE/$INPUT_OUTPUTSPATH"
+npm run export -- -o $GITHUB_WORKSPACE/$INPUT_OUTPUTSPATH
+
+# Commit the changes.
+cd $GITHUB_WORKSPACE
+git config user.name github-actions
+git config user.email github-actions@github.com
+git add $INPUT_OUTPUTS_PATH
+git commit -m "Generated build from workflow run: $GITHUB_RUN_ID"
+git push
--- test.sh
+++ test.sh
@@ -11,6 +11,7 @@ docker run \
   -e INPUT_REPORTSPATH=./.reports \
   -e INPUT_OUTPUTSPATH=./docs \
   -e GITHUB_WORKSPACE=$workdir \
+  -e GITHUB_RUN_ID=test \
   -w=$workdir \
   --mount type=bind,src=$(pwd),dst=$workdir \
   --rm -it $tag

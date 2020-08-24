1570cb73210ea709b9577c06084db5b4e7cb6ff6
Jonathan Clem <jclem@github.com>
2020-08-24 13:27:20 -0400

    Revert "Fix input/outputs names"
    This reverts commit ecaa1e1fff9a95f66898f091aae1c1dfd4fe3812.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,19 +5,19 @@ set -ve
 build_dir=/opt/build
 
 # Copy the reports from the working directory to our build location.
-cp -R $INPUT_REPORTS_PATH $build_dir/.reports
+cp -R $INPUT_REPORTSPATH $build_dir/.reports
 # Move the outputs path to the build location so we can use the cached build.
-mv $GITHUB_WORKSPACE/$INPUT_OUTPUTS_PATH $build_dir
+mv $GITHUB_WORKSPACE/$INPUT_OUTPUTSPATH $build_dir
 
 # Export back to the workspace output path.
 cd $build_dir
 npm run build
-npm run export -- -o $GITHUB_WORKSPACE/$INPUT_OUTPUTS_PATH
+npm run export -- -o $GITHUB_WORKSPACE/$INPUT_OUTPUTSPATH
 
 # Commit the changes.
 cd $GITHUB_WORKSPACE
 git config user.name github-actions
 git config user.email github-actions@github.com
-git add $INPUT_OUTPUTS_PATH
+git add $INPUT_OUTPUTSATH
 git commit -m "Generated build from workflow run: $GITHUB_RUN_ID"
 git push

7504f817866df0bbff3e7a28cef5d890cd5e8617
Jonathan Clem <jclem@github.com>
2020-08-27 14:03:03 -0400

    Add npm style block report

---
+++ components/ReportCard.tsx
@@ -0,0 +1,112 @@
+import classnames from 'classnames'
+import styles from './ReportCard.module.css'
+
+type Stats = {
+  proposed: number
+  accepted: number
+  inProgress: number
+  done: number
+  warn: number
+  alert: number
+  activeGt3Wk: number
+  noTarget: number
+  pastTarget: number
+}
+
+type Props = Stats & {totals: Stats}
+
+export default function ReportCard(props: Props) {
+  const Square = (props: {
+    title: string
+    value: number
+    total?: number
+    warn?: boolean | null
+    alert?: boolean | null
+    width?: string | null
+    height?: string | null
+    halfSize?: boolean
+  }) => {
+    const classNames = classnames({
+      [styles.square]: true,
+      [styles.halfSize]: props.halfSize,
+      [styles.warn]: props.warn,
+      [styles.alert]: props.alert
+    })
+
+    return (
+      <div className={classNames}>
+        <span className={styles.title}>{props.title}</span>
+
+        <div className={styles.value}>
+          <span>{props.value}</span>
+          {props.total != null && (
+            <span className={styles.total}> / {props.total}</span>
+          )}
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <div className={styles.reportCard}>
+      <Square
+        title="Proposed"
+        value={props.proposed}
+        total={props.totals.proposed}
+      />
+
+      <Square
+        title="Accepted"
+        value={props.accepted}
+        total={props.totals.accepted}
+      />
+
+      <Square
+        title="In Progress"
+        value={props.inProgress}
+        total={props.totals.inProgress}
+      />
+
+      <div className="w-40 h-40 grid grid-rows-2 grid-cols-1 gap-2">
+        <Square
+          title="Warning"
+          value={props.warn}
+          warn={props.warn > 0}
+          total={props.totals.warn}
+          halfSize
+        />
+
+        <Square
+          title="Alert"
+          value={props.alert}
+          total={props.totals.alert}
+          alert={props.alert > 0}
+          halfSize
+        />
+      </div>
+
+      <Square
+        title="Active > 3 Weeks"
+        value={props.activeGt3Wk}
+        warn={props.activeGt3Wk > 0}
+        total={props.totals.activeGt3Wk}
+      />
+
+      <Square
+        title="Past Target Date"
+        value={props.pastTarget}
+        warn={props.pastTarget > 0}
+        total={props.totals.pastTarget}
+      />
+
+      <Square
+        title="No Target Date"
+        value={props.noTarget}
+        warn={props.noTarget > 0}
+        total={props.totals.noTarget}
+      />
+
+      <Square title="Done" value={props.done} total={props.totals.done} />
+    </div>
+  )
+}
--- components/sections/ProjectGroupbyStatus.tsx
+++ components/sections/ProjectGroupbyStatus.tsx
@@ -6,24 +6,29 @@ import {useMemo} from 'react'
 import {CellProps, Column} from 'react-table'
 import DataWithFlag from '../DataWithFlag'
 import NullData from '../NullData'
+import ReportCard from '../ReportCard'
 import SectionTitle from '../SectionTitle'
 import Table from '../Table'
 
 type Props = ProjectGroupbyStatusData
-type StatusGroup = {key: string; group: Total}
+type StatusGroup = {key: string; totals: Total}
 
 const TOTAL_KEY = 'Total'
 
 export default function ProjectGroupbyStatus(props: Props) {
-  const groups = Object.entries(props.groups).map(([key, group]) => ({
-    key,
-    group
-  }))
+  const groups: StatusGroup[] = Object.entries(props.groups).map(
+    ([key, group]) => ({
+      key,
+      totals: group
+    })
+  )
 
-  groups.push({
+  const totals = {
     key: TOTAL_KEY,
-    group: props.total
-  })
+    totals: props.total
+  }
+
+  const tableGroups = [...groups, totals]
 
   // TODO: What do yellow/red hearts actually mean?
 
@@ -36,7 +41,7 @@ export default function ProjectGroupbyStatus(props: Props) {
       {
         Header: 'Limit',
         id: 'limit',
-        accessor: row => row.group.stages.inProgressLimits.limit,
+        accessor: row => row.totals.stages.inProgressLimits.limit,
         Cell: ({row, cell}: CellProps<StatusGroup, number | null>) => {
           // We don't display total for the "Total" row, it's considered
           // meaningless.
@@ -50,19 +55,19 @@ export default function ProjectGroupbyStatus(props: Props) {
       {
         Header: 'Proposed',
         id: 'proposed',
-        accessor: row => row.group.stages.proposed.length
+        accessor: row => row.totals.stages.proposed.length
       },
       {
         Header: 'Accepted',
         id: 'accepted',
-        accessor: row => row.group.stages.accepted.length
+        accessor: row => row.totals.stages.accepted.length
       },
       {
         Header: 'In Progress',
         id: 'inProgress',
-        accessor: row => row.group.stages.inProgress.length,
+        accessor: row => row.totals.stages.inProgress.length,
         Cell: ({row, cell}: CellProps<StatusGroup, number | null>) => (
-          <DataWithFlag flag={row.original.group.stages.inProgressLimits.flag}>
+          <DataWithFlag flag={row.original.totals.stages.inProgressLimits.flag}>
             {cell.value}
           </DataWithFlag>
         )
@@ -70,40 +75,40 @@ export default function ProjectGroupbyStatus(props: Props) {
       {
         Header: 'Done',
         id: 'done',
-        accessor: row => row.group.stages.done.length
+        accessor: row => row.totals.stages.done.length
       },
       {
         Header: '💛',
         id: 'yellow',
-        accessor: row => row.group.flagged.yellow.length,
+        accessor: row => row.totals.flagged.yellow.length,
         Cell: ({cell}: CellProps<StatusGroup, number | null>) =>
           cell.value ?? <NullData />
       },
       {
         Header: '❤️',
         id: 'red',
-        accessor: row => row.group.flagged.red.length,
+        accessor: row => row.totals.flagged.red.length,
         Cell: ({cell}: CellProps<StatusGroup, number | null>) =>
           cell.value ?? <NullData />
       },
       {
         Header: 'Active > 3 wks',
         id: 'activeGt3Wk',
-        accessor: row => row.group.flagged.inProgressDuration.length,
+        accessor: row => row.totals.flagged.inProgressDuration.length,
         Cell: ({cell}: CellProps<StatusGroup, number | null>) =>
           cell.value ?? <NullData />
       },
       {
         Header: 'No Target Date',
         id: 'noTarget',
-        accessor: row => row.group.flagged.noTarget.length,
+        accessor: row => row.totals.flagged.noTarget.length,
         Cell: ({cell}: CellProps<StatusGroup, number | null>) =>
           cell.value ?? <NullData />
       },
       {
         Header: 'Past Target Date',
         id: 'pastTarget',
-        accessor: row => row.group.flagged.pastTarget.length,
+        accessor: row => row.totals.flagged.pastTarget.length,
         Cell: ({cell}: CellProps<StatusGroup, number | null>) =>
           cell.value ?? <NullData />
       }
@@ -113,7 +118,36 @@ export default function ProjectGroupbyStatus(props: Props) {
   return (
     <>
       <SectionTitle icon="🚀">Project Execution</SectionTitle>
-      <Table columns={columns} data={groups} />
+
+      {groups.map(group => (
+        <div className="flex items-start mb-8" key={group.key}>
+          <h1 className="w-56">{group.key}</h1>
+
+          <ReportCard
+            proposed={group.totals.stages.proposed.length}
+            accepted={group.totals.stages.accepted.length}
+            inProgress={group.totals.stages.inProgress.length}
+            done={group.totals.stages.done.length}
+            warn={group.totals.flagged.yellow.length}
+            alert={group.totals.flagged.red.length}
+            activeGt3Wk={group.totals.flagged.inProgressDuration.length}
+            noTarget={group.totals.flagged.noTarget.length}
+            pastTarget={group.totals.flagged.pastTarget.length}
+            totals={{
+              proposed: totals.totals.stages.proposed.length,
+              accepted: totals.totals.stages.accepted.length,
+              inProgress: totals.totals.stages.inProgress.length,
+              done: totals.totals.stages.done.length,
+              warn: totals.totals.flagged.yellow.length,
+              alert: totals.totals.flagged.red.length,
+              activeGt3Wk: totals.totals.flagged.inProgressDuration.length,
+              noTarget: totals.totals.flagged.noTarget.length,
+              pastTarget: totals.totals.flagged.pastTarget.length
+            }}
+          />
+        </div>
+      ))}
+      <Table columns={columns} data={tableGroups} />
     </>
   )
 }
